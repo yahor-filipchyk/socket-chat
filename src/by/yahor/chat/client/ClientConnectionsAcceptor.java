83d4a4c0d9d1d2f16de2d01c8a5e98c1b0fecae2
Yahor <yahor.filipchyk@gmail.com>
2013-10-23 23:17:31 +0300

    First commit

--- src/by/yahor/chat/client/ClientConnectionsAcceptor.java
+++ src/by/yahor/chat/client/ClientConnectionsAcceptor.java
@@ -4,10 +4,17 @@
  */
 package by.yahor.chat.client;
 
+import by.yahor.chat.server.NetPeer;
+import by.yahor.chat.utils.AddressResolverUtils;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import my.yahorfilipchyk.console.Console;
 
 /**
  *
@@ -16,7 +23,9 @@ import java.util.Random;
 public class ClientConnectionsAcceptor extends Thread {
     
     private int port;
+    private boolean closed = true;
     private ServerSocket connectionAcceptor;
+    private List<ConversationListener> listeners = new ArrayList<>();;
     
     public ClientConnectionsAcceptor() {
         bindAcceptor();
@@ -24,11 +33,59 @@ public class ClientConnectionsAcceptor extends Thread {
     
     @Override
     public void run() {
-        try {
-            Socket client = connectionAcceptor.accept();
-        } catch (IOException ex) {
-            ex.printStackTrace();;
+        while (!closed) {
+            if (!connectionAcceptor.isBound()) {
+                bindAcceptor();
+            }
+            try {
+                Console.writeLine("Trying to accept connection");
+                Socket client = connectionAcceptor.accept();
+                Console.writeLine("Connection accepted");
+                if (client != null && client.isConnected() && client.isBound()) {
+                    NetPeer peer = new NetPeer(client);
+                    for (ConversationListener listener : listeners) {
+                        Conversation conversation = new Conversation(peer);
+                        listener.conversationCreated(conversation);
+                    }
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();;
+            }
         }
+    }
+    
+    public byte[] getAddress() {
+        InetAddress address = AddressResolverUtils.getLocalAddress();
+        if (address != null) {
+            return address.getAddress();
+        } else {
+            try {
+                address = InetAddress.getLocalHost();
+                return address.getAddress();
+            } catch (UnknownHostException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return connectionAcceptor.getInetAddress().getAddress();
+    }
+    
+    public int getPort() {
+        return connectionAcceptor.getLocalPort();
+    }
+    
+    public void addConversationListener(ConversationListener listener) {
+        this.listeners.add(listener);
+    }
+    
+    public void close() {
+        if (connectionAcceptor != null) {
+            try {
+                connectionAcceptor.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        closed = true;
     }
     
     private void bindAcceptor() {
@@ -37,9 +94,16 @@ public class ClientConnectionsAcceptor extends Thread {
             try {
                 Random portGenerator = new Random(System.currentTimeMillis());
                 port = portGenerator.nextInt(65535 - 1024) + 1024;
+                Console.writeLine("trying port: " + port);
                 connectionAcceptor = new ServerSocket(port);
+//                byte[] ipv4 = AddressResolverUtils.getLocalAddress().getAddress();
+//                for (int i = 0; i < ipv4.length; i++) {
+//                    Console.write("" + (byte) ipv4[i] + (i == ipv4.length - 1 ? "\n" : "."));
+//                }
+                Console.writeLine("isBound: " + connectionAcceptor.isBound());
                 if (connectionAcceptor.isBound()) {
                     binded = true;
+                    closed = false;
                 }
             } catch (IOException ex) {
                 ex.printStackTrace();
